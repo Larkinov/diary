48380d35d63e0088d8daca3fda14e35444faa717
const <const9501.dev@gmail.com>
2023-12-28 18:18:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="source/style.css">
+    <title>Дневник</title>
+</head>
+
+<body>
+    <header>
+        <div class="header__container">
+            <h2 class="logo">
+                Simple Dialy
+            </h2>
+            <div class="iconBlock">
+                <img src="./source/img/info.png" alt="settings" class="iconBlock__icon">
+                <img src="./source/img/settings.png" alt="settings" class="iconBlock__icon">
+            </div>
+        </div>
+
+    </header>
+    <div class="content">
+        <div class="sidebar">
+
+        </div>
+        <div class="notepad"></div>
+    </div>
+
+    <footer>
+        <p>*getYear*©</p>
+    </footer>
+</body>
+
+</html>
